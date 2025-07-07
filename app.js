74d061df4917afbe937f069ea8f1f1c11c1c5924
Jayancv <jcvidanapathirana@gmail.com>
2025-07-07 23:35:39 +0300

    Health check issue fix

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  if(req) throw('Error!')
   res.send('ok')
 })
 
